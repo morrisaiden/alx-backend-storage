ba3f8a0a215ed34e7647b0b00f5f836c6efdba2a
morrisaiden <mwaimorris1997@gmail.com>
2024-08-08 08:23:49 +0000

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Creates table users with unique identifiers --
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
