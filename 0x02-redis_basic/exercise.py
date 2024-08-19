acaec790b4eb19751a8aa9918d9ad769bc883887
morrisaiden <mwaimorris1997@gmail.com>
2024-08-19 15:13:13 +0300

    Retrieving

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -42,7 +42,12 @@ def replay(method: Callable):
     print(f"{method.__qualname__} was called {len(inputs)} times:")
 
     for input_args, output in zip(inputs, outputs):
-        print(f"{method.__qualname__}(*{input_args.decode('utf-8')}) -> {output.decode('utf-8')}")
+        print(f"
+              {method.__qualname__}(*{
+                  input_args.decode('utf-8')
+                  }) -> {
+                      output.decode('utf-8')
+                      }")
 
 
 class Cache:
