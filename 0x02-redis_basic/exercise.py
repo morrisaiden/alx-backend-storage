7dba743f40478fc804ed056a1de0f5c3a12b1e9d
morrisaiden <mwaimorris1997@gmail.com>
2024-08-19 14:58:57 +0300

    Git add

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,6 @@ import uuid
 from typing import Union, Callable, Optional
 import functools
 
-
 def count_calls(method: Callable) -> Callable:
     """Decorator to count the number of times a method is called."""
     @functools.wraps(method)
@@ -14,13 +13,26 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs of a function."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        # Store the inputs
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls  # Decorate the store method
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
